001296c635eb6272738b2a97a2e674e092c7873d
montanizstills <montanizstills@gmail.com>
2025-04-01 13:15:51 -0400

    -save tensors to file for faster processing;

--- src/main/java/io/github/ailearner/MultiLayerPerceptionExample.java
+++ src/main/java/io/github/ailearner/MultiLayerPerceptionExample.java
@@ -4,10 +4,12 @@ import io.github.ailearner.utils.FileHandler;
 import io.github.ailearner.utils.NNTrainingDataHelper;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.nd4j.common.resources.strumpf.ResourceFile;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.function.Function;
@@ -34,11 +36,11 @@ public class MultiLayerPerceptionExample {
         // read all words from names.txt
         fh.readWordsFromFile(file_path).forEach((word) -> {
             word = word + "."; // add END_TOKEN
-            // System.out.println("=============");
-            // System.out.printf("Word: %s\n", word);
+//             System.out.println("=============");
+//             System.out.printf("Word: %s\n", word);
             final String[] lettersOfCurrentWord = word.split("");
             IntStream.range(0, (lettersOfCurrentWord.length)).forEach(chxInWord -> {
-                // System.out.printf("Given: %s --> Expect: %s\n", xContext, charMap_to_IdxSupplier.apply(lettersOfCurrentWord[chxInWord]));
+                System.out.printf("Given: %s --> Expect: %s\n", xContext, charMap_to_IdxSupplier.apply(lettersOfCurrentWord[chxInWord]));
                 int idx = charMap_to_IdxSupplier.apply(lettersOfCurrentWord[chxInWord]);
                 tensorX.add(xContext.dup()); // add dup to avoid changing underlying array later in program
                 tensorY.add(charMap_to_IdxSupplier.apply(lettersOfCurrentWord[chxInWord]));
@@ -57,68 +59,117 @@ public class MultiLayerPerceptionExample {
         // long endTime = System.currentTimeMillis();
         // System.out.printf("Created Nd4j TensorX in %d ms\n", endTime - startTime);
         INDArray dl4jTensorY = Nd4j.create(tensorY);
+
+        writeTensorToBinaryFile(dl4jTensorX, "/src/main/java/resources/tensorX.bin");
+        writeTensorToBinaryFile(dl4jTensorY, "/src/main/java/resources/tensorY.bin");
         return new ImmutablePair<>(dl4jTensorX, dl4jTensorY);
+    }
+
+    public void writeTensorToBinaryFile(INDArray tensor, String filePath) {
+        try {
+            Nd4j.saveBinary(
+                    tensor,
+                    new File(System.getProperty("user.dir") + filePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ImmutablePair<INDArray, INDArray> loadTensorFromBinaryFile(String filePath) {
+        try {
+            INDArray loadedTensorX = Nd4j.readBinary(new File(filePath + "_tensorX.bin"));
+            INDArray loadedTensorY = Nd4j.readBinary(new File(filePath + "_tensorY.bin"));
+
+            // Alternative: load compressed format
+            // INDArray loadedTensorX = Nd4j.readCompressed(new FileInputStream(filePath + "_tensorX.bin.z"));
+            // INDArray loadedTensorY = Nd4j.readCompressed(new FileInputStream(filePath + "_tensorY.bin.z"));
+
+            return new ImmutablePair<>(loadedTensorX, loadedTensorY);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static void main(String[] args) {
         MultiLayerPerceptionExample mlp = new MultiLayerPerceptionExample();
-        // long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         Pair<INDArray, INDArray> XY = mlp.prepareTrainingData(System.getProperty("user.dir") + "/src/main/java/resources/names.txt");
-        // long endTime = System.currentTimeMillis();
-        // System.out.printf("Training Completed in %d ms\n", endTime - startTime);
+        long endTime = System.currentTimeMillis();
+        System.out.printf("Training Completed in %s ms\n", endTime - startTime);
+        mlp.train();
     }
 
+
+    /**
+     * Goal: For the given context we want to predict the next Y.
+     * <p>
+     * Forward Pass:
+     * 1. Take C[X] and embed (fully-connected) in Layer1 (*hyper parameter); Or, C[X[i]];
+     * Create a vector of embeddings:
+     * In Bengio et. 2003, the designers uses 3 word embeddings.
+     * (https://www.jmlr.org/papers/volume3/bengio03a/bengio03a.pdf)
+     * (https://github.com/karpathy/makemore/blob/master/makemore.py)
+     * <p>
+     * If we take all the data set (as potential samples of f(x)),
+     * Then the lookup matrix, C, looks like:
+     * C.shape() = (len(all_chars_x)+'.', 1) or ('a,...,z,<.>', 1)
+     * And, the embedding vector for the given context looks like: C[X];
+     * If we want a specific char, say 'a', then the embedding vector looks like: C[X[i]] = C['a'];
+     * <p>
+     * <p>
+     * 2. Take Layer2=tanh(Layer1), fully-connected (matrix) to C,
+     * Take Layer2=tanh(Layer1), fully-connected (matrix) to C for non-linearity;
+     * <p>
+     * <p>
+     * <p>
+     * 3. Take Softmax(Layer2) -> tokenOut; Or, yield(): sample/expected output; Or, P(w_i | context);
+     * <p>
+     * <p>
+     * **Note:
+     * Context comes in as stream. Y is the next expected char in the stream. Aka, MultiHeadAttention.
+     * Hyper Parameters: Train at different values to reduce loss;
+     * We use random vector inits to help avoid linearity.
+     */
     void train() {
         //Prepare Training Data
-        prepareTrainingData("file/path/names.txt");
+        StringBuilder content = new StringBuilder();
+        String path = "src/main/java/resources/names.txt";
+        InputStream inputStream = getClass().getResourceAsStream(path);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println(content.toString());
+        System.exit(1);
+        Pair<INDArray, INDArray> XY = prepareTrainingData("file/path/names.txt");
+
+
         // this.run(training = true, sampleSize = 10000); // if training==true, sampleSize = epochs;
         // loss function and backpropagation
         // loss = Nd4j.nn().lossFunction(layer2, vectorY);
         // Nd4j.loss().softmaxCrossEntropy(null,vectorY, 0.1);
 
-        /**
-         * Goal: For the given context we want to predict the next Y.
-         *
-         * Forward Pass:
-         * 1. Take C[X] and embed (fully-connected) in Layer1 (*hyper parameter); Or, C[X[i]];
-         *  Create a vector of embeddings:
-         *              In Bengio et. 2003, the designers uses 3 word embeddings.
-         *              (https://www.jmlr.org/papers/volume3/bengio03a/bengio03a.pdf)
-         *              (https://github.com/karpathy/makemore/blob/master/makemore.py)
-         *
-         *              If we take all the data set (as potential samples of f(x)),
-         *              Then the lookup matrix, C, looks like:
-         *                  C.shape() = (len(all_chars_x)+'.', 1) or ('a,...,z,<.>', 1)
-         *              And, the embedding vector for the given context looks like: C[X];
-         *              If we want a specific char, say 'a', then the embedding vector looks like: C[X[i]] = C['a'];
-         *
-         *
-         * 2. Take Layer2=tanh(Layer1), fully-connected (matrix) to C,
-         *  Take Layer2=tanh(Layer1), fully-connected (matrix) to C for non-linearity;
-         *
-         *
-         *
-         * 3. Take Softmax(Layer2) -> tokenOut; Or, yield(): sample/expected output; Or, P(w_i | context);
-         *
-         *
-         * **Note:
-         *  Context comes in as stream. Y is the next expected char in the stream. Aka, MultiHeadAttention.
-         *  Hyper Parameters: Train at different values to reduce loss;
-         *  We use random vector inits to help avoid linearity.
-         */
 
         Integer contextLength = 3; // amount of chars used to predict next one;
         Integer numberOfNeurons = 100; // we want to fully connect all first layer input neurons to this;
         Integer dimsToSqueeze = 2; // squeeze the first layer input to this dimension;
 
         // get all chars from the names.txt file;
-        ArrayList allCharsX = null; //mlp.prepareTrainingData("/file/path/names.txt");
+        // ArrayList allCharsX = null; //mlp.prepareTrainingData("/file/path/names.txt");
 
         // "first layer input" - input to model/neural-net
         // is a vectorX with dims: [len(all_chars_x), hyper_parameter]
         // and, should start with as zero tensor; then we will fill it with the context;
 
-        INDArray vectorX = Nd4j.zeros(allCharsX.size(), dimsToSqueeze);
+        INDArray vectorX = Nd4j.zeros(XY.getLeft().length(), dimsToSqueeze);
 
         // first layer output vector: in(contextLength) => out(numberOfSecondLayerInputNeurons)
         INDArray vectorY = Nd4j.rand(contextLength, numberOfNeurons);
